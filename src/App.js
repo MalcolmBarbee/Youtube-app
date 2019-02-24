849e1cd7a2278d4f6e75edbcbb285cffdb0e95e3
Van Tran <vantran@pursuit.org>
2019-02-24 15:18:39 -0500

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        className="App">
-   
+      <div className="App">
+      home page
       </div>
     );
   }
