9e3954d990397242cb71dd7830d98e686fe344fa
Hervey John Lamothe <herveylamothe@gmail.com>
2019-02-24 17:11:32 -0500

    added import/export stuff  on app.js aNd feedtitle.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import feedTitle from './components/feedTitle';
 
 
 class App extends Component {
--- src/components/feedTitle.js
+++ src/components/feedTitle.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './feedTitle.css';
 
 class feedTitle extends Component {
   render() {
     return (
       <div>
+    <div class="container">
+  <!-- Content here -->
+    </div>
         <h1>Your Personalized Feed</h1>
         <p>Pursuit Tube</p>
    
@@ -12,5 +15,6 @@ class feedTitle extends Component {
     );
   }
 }
+
 
 export default feedTitle;
