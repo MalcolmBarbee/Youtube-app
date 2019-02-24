92506e3c3cf771feac2364a66501c998193152f1
Hervey John Lamothe <herveylamothe@gmail.com>
2019-02-24 16:17:24 -0500

    createdFeedTitle js and css

---
+++ src/components/feedTitle.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './feedTitle.css';
+
+class feedTitle extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Your Personalized Feed</h1>
+        <p>Pursuit Tube</p>
+   
+      </div>
+    );
+  }
+}
+
+export default feedTitle;
