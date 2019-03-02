15002cc14ace712bb76aeb484648b0b2a36a9e47
Hervey John Lamothe <herveylamothe@gmail.com>
2019-03-02 14:44:19 -0500

    added cb function that responds for every type in search bar

--- src/containers/feededitor.js
+++ src/containers/feededitor.js
@@ -4,18 +4,31 @@ class feedEditor extends Component {
   constructor(props) {
     super(props)
     this.state={
-      userFeed: [
-        "Mo's Feed",
+      searchInput: "",
+      usersFeed: [{
+        name: "Hervey",
+        feed:["food", "comics"]
+      }
+        
+        // "Mo's Feed",
         
       ]
     }
   }
+  //on change input handle change
+  //console.log everyrhting typed
   render() {
     return (
+      <>
       <div>
         <form>
           <h2>Create new Feed</h2>
           <input type="text"
+          onChange={
+            (e)=>{
+              console.log(e)
+            }
+          }
           placeholder="Feed Name"
           />
           <button onClick={function(e){e.preventDefault();
@@ -24,7 +37,7 @@ class feedEditor extends Component {
         <h2>Feed List</h2>
         <ul>
           {
-            this.state.userFeed.map(
+            this.state.usersFeed.map(
               userFeed=><li>
                 userFeed
               </li>)
@@ -32,6 +45,7 @@ class feedEditor extends Component {
           }
         </ul>
       </div>
+      </>
     );
   }
 }
