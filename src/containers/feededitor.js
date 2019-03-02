b6ba17e7fbdf174a72c871bea6ee10c737392e4f
Hervey John Lamothe <herveylamothe@gmail.com>
2019-03-02 12:31:57 -0500

    Started feed editor hard code

--- src/containers/feededitor.js
+++ src/containers/feededitor.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
  
 class feedEditor extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      userFeed: [
+        "Mo's Feed",
+        
+      ]
+    }
+  }
   render() {
     return (
       <div>
-      
+        <form>
+          <h2>Create new Feed</h2>
+          <input type="text"
+          placeholder="Feed Name"
+          />
+          <button onClick={function(e){e.preventDefault();
+         console.log("Button has been clicked")}}>Add</button>
+        </form>
+        <h2>Feed List</h2>
+        <ul>
+          {
+            this.state.userFeed.map(
+              userFeed=><li>
+                userFeed
+              </li>)
+            
+          }
+        </ul>
       </div>
     );
   }
